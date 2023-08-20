aa0b9660b50fe683c9f021cfeaf9ad723bef187c
Timothy Phan <timdttp@gmail.com>
2023-08-20 13:03:15 -0500

    Finished

--- exercises/enterName/main.cpp
+++ exercises/enterName/main.cpp
@@ -1,20 +1,46 @@
+#include <algorithm> // for std::sort
 #include <iostream>
+#include <string> // for std::string
 
 // function that inputs a name and returns the name to main()
+std::string userInput(int iter)
+{   
+    // iter + 1 so that program starts count at 1 not 0
+    std::cout << "Enter name #" << iter + 1 << ": ";
+    std::string name{};
+    std::cin >> name;
 
-// function prints array (loop)
+    return name;
+}
 
 int main()
 {
     // ask user for number of names
     std::cout << "How many names would you like to enter? ";
-    int lenNames{};
+    int lenNames{}; // implicit conversion to std::size_t
     std::cin >> lenNames;
 
+
     // loops for next name + add to array
+    std::string* array{ new std::string[lenNames]{} };
     for (int i{ 0 }; i < lenNames; ++i)
     {
-        std::cout >> i >> '\n';
+        array[i] = userInput(i);
+    }
+    std::cout << '\n';
+
+
+    // sort array
+    // Cannot use std::begin + std::end
+    std::sort(array, (array + lenNames));
+    
+
+    // print array
+    std::cout << "Here is your sorted list:" << '\n';
+    for (int i{ 0 }; i < lenNames; ++i)
+    {
+        std::cout << "Name #" << i + 1 << ": ";
+        std::cout << array[i] << '\n';
     }
 
     return 0;
