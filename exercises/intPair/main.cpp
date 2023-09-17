391bb1318cc356ceaf4c3ff9ac279fd1b1d6a8a5
TimDTTP <timdttp@gmail.com>
2023-09-17 15:14:51 -0500

    Starting Problem #2

--- exercises/intPair/main.cpp
+++ exercises/intPair/main.cpp
@@ -20,5 +20,8 @@ int main()
 	std::cout << "p2: ";
 	p2.print();
 
+	std::cout << "p1 and p1 " << (p1.isEqual(p1) ? "are equal\n" : "are not equal\n");
+	std::cout << "p1 and p2 " << (p1.isEqual(p2) ? "are equal\n" : "are not equal\n");
+	
 	return 0;
 }
