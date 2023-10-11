a4db71802c89b240e5b8432be660de8dc85f8cdd
TimDTTP <timdttp@gmail.com>
2023-10-11 11:36:19 -0500

    Finished

--- exercises/ballClass/main.cpp
+++ exercises/ballClass/main.cpp
@@ -8,28 +8,16 @@ private:
     double m_radius{ 10 };
 
 public:
-    Ball()
-    {
-        Ball{ "black", 10 };
-    }
-
-    Ball(std::string_view color)
-        : m_color{ color }
-    {
-        Ball{ color, 10 };
-    }
-
     Ball(double radius)
-        : m_radius{ radius }
+        : Ball{ "black", radius }
     {
-        Ball{ "black", radius };
     }
-
-    Ball(std::string_view color, double radius)
+    
+    Ball(std::string_view color="black", double radius=10.0)
         : m_color{ color }
         , m_radius{ radius }
     {
-        std::cout << "Ball(" << m_color << ", " << radius << ")\n";
+        std::cout <<  "Ball(" << m_color << ", " << m_radius << ")\n";
     }
 };
 
