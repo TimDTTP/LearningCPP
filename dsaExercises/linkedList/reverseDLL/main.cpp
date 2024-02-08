66df9e194dcb4a3766ff79b7dfbcadb566f5015f
TimDTTP <timdttp@gmail.com>
2024-02-08 13:56:18 -0600

    Finished

--- dsaExercises/linkedList/reverseDLL/main.cpp
+++ dsaExercises/linkedList/reverseDLL/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <stack>
 
 class Node {
 public:
@@ -21,6 +22,24 @@ public:
         }
 
         *headRef = newNode;
+    }
+
+    void append(class Node** headRef, int data) {
+        class Node* newNode = new Node();
+        newNode->data = data;
+        newNode->next = NULL;
+        class Node* last{*headRef};
+
+        if (*headRef == NULL) {
+            *headRef = newNode;
+            return;
+        }
+
+        while (last->next != NULL) {
+            last = last->next;
+        }
+        last->next = newNode;
+        return;
     }
 
     void printList(class Node* node) {
@@ -56,30 +75,67 @@ char userInput() {
     return userChoice;
 }
 
-class Node* bruteForce() {
+class Node* bruteForce(class Node* headRef) {
+    class Node* temp{headRef};
 
+    while (headRef != NULL) {
+        class Node* nextHolder{headRef->next};
+        
+        headRef->next = headRef->prev;
+        headRef->prev = nextHolder;
+
+        if ((headRef->prev) == NULL) {
+            return headRef;
+        }
+        else {
+            headRef = headRef->prev; 
+        }
+    }
+
+    return headRef;
 }
 
-class Node* stack() {
-
-}
-
-void chooseApproach() {
-    char choice{userInput()};
+class Node* stack(class Node* headRef) {
+    std::stack<int> numStack{};
+    class Node* newHead{NULL};
     class NodeOperation* temp = new NodeOperation();
+
+    // add to stack
+    while (headRef != NULL) {
+        numStack.push(headRef->data);
+
+        headRef = headRef->next;
+    }
+
+    // append to new linked list
+    while (numStack.size() != 0) {
+        temp->append(&newHead, numStack.top());
+        numStack.pop();
+    }
+
+    return newHead;
+}
+
+void solution(class Node* headRef) {
+    class NodeOperation* temp = new NodeOperation();
+    std::cout << "The following is the linked list: " << std::endl;
+    temp->printList(headRef);
+    std::cout << '\n';
+
+    char choice{userInput()};
     
     std::cout << "The result of approach ";
 
     switch (choice) {
         case('A') : {
             std::cout << "A:" << std::endl;
-            temp->printList(bruteForce());
+            temp->printList(bruteForce(headRef));
             break;
         }
 
         case('B') : {
             std::cout << "B:" << std::endl;
-            temp->printList(stack());
+            temp->printList(stack(headRef));
             break;
         }
     }
@@ -97,7 +153,7 @@ int main() {
     }
 
     // start of solution
-    
+    solution(head);
 
     return 0;
 }
