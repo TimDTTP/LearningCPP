32686e152482de54eb1ef653895e45cdd56d0385
TimDTTP <timdttp@gmail.com>
2024-03-06 17:00:07 -0600

    Nested loop approach completed
    Completed 1/3 approaches

--- dsaExercises/arrays/maxProduct/main.cpp
+++ dsaExercises/arrays/maxProduct/main.cpp
@@ -1,76 +1,55 @@
 
+#include <algorithm>
 #include <array>
 #include <iostream>
-#include <limits>
 
-// print array
-void printArray(const std::array<int, 7> &arr) {
-  for (const int i : arr) {
-    std::cout << i << ' ';
-  }
-  std::cout << std::endl;
-}
+class Solution {
+private:
+  // Nested loop approach
+  int nestedLoop(std::array<int, 7> arr) {
+    int size{arr.size()};
+    int maxSoFar{};
+    int temp{};
 
-// print user prompt
-void prompt(const std::array<int, 7> &arr) {
-  std::cout << "Given array: ";
-  printArray(arr);
-  std::cout << "Choose one of the following approaches: " << std::endl;
-
-  std::cout << "A) Nest loops" << '\n'
-            << "B) Kadane's algorithm" << '\n'
-            << "C) Traversal" << '\n'
-            << std::endl;
-}
-
-// get user input
-char getUserInput() {
-  char input{};
-  std::cout << "Enter: ";
-  std::cin >> input;
-
-  if (std::cin) {
-    if (!std::cin.eof())
-      exit(0);
-
-    std::cin.clear();
-  }
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-  return input;
-}
-
-// Nested loop approach
-int nestedLoop(std::array<int, 7> arr, int size) {
-  int maxSoFar{};
-  int temp{};
-
-  for (int i{0}; i < (size - 1); ++i) {
-    temp = arr[i];
-    for (int j{i}; i < (size - 1); ++j) {
-      if (arr[j] == 0)
-        break;
-      else
-        temp *= arr[i];
-
+    for (int i{0}; i < size; ++i) {
+      temp = arr[i];
+      for (int j{i + 1}; j < size; ++j) {
+        std::cout << arr[j] << std::endl;
+        if (arr[j] == 0)
+          break;
+        else
+          temp *= arr[j];
+      }
       maxSoFar = std::max(temp, maxSoFar);
     }
+
+    return maxSoFar;
   }
 
-  return maxSoFar;
-}
+  // Kadanes algorithm approach
 
-// Kadanes algorithm approach
+  // Traversal approach
 
-// Traversal approach
+public:
+  // Prints array
+  void printArray(const std::array<int, 7> &arr) {
+    for (const int i : arr) {
+      std::cout << i << ' ';
+    }
+    std::cout << std::endl;
+  }
+
+  // Returns results using all approaches
+  void printSolution(const std::array<int, 7> &arr) {
+    std::cout << "Approach A)" << nestedLoop(arr) << std::endl;
+  }
+};
 
 int main() {
   std::array<int, 7> arr{4, 8, -2, 0, 5, -9, -4};
 
-  char input{};
-
-  prompt(arr);
-  input = getUserInput();
+  Solution cursor = Solution();
+  cursor.printSolution(arr);
 
   return 0;
 }
