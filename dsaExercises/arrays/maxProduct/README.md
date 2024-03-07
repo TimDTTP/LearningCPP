b1e77da5730aacf77f167689f7754e9e462049e5
TimDTTP <timdttp@gmail.com>
2024-03-07 10:28:41 -0600

    Problem Closed
    Completed 3/3 approaches
    Problem closed

--- dsaExercises/arrays/maxProduct/README.md
+++ dsaExercises/arrays/maxProduct/README.md
@@ -17,4 +17,11 @@ Given an array containing both positive and negative integers; find the largest 
    - One variable will keep the max thus far for the lifetime of the function
    - Return overall max
 3. Traversal from both ends
-
+  - Traverse twice from both sides
+    - Must track from both sides in order due to rejection of last negative number in case of odd
+  - Have 2 trackers
+    - One to track current product
+    - One to track max thus far
+  - Each time, if encounter '0', product = 1
+    - To negate 0 multiplication
+  - Return max
--- dsaExercises/arrays/maxProduct/main.cpp
+++ dsaExercises/arrays/maxProduct/main.cpp
@@ -1,3 +1,4 @@
+// NOTE: Reminder to stash changes, could not pull 18:26 03/06/24
 
 #include <algorithm>
 #include <array>
@@ -14,7 +15,6 @@ private:
     for (int i{0}; i < size; ++i) {
       temp = arr[i];
       for (int j{i + 1}; j < size; ++j) {
-        std::cout << arr[j] << std::endl;
         if (arr[j] == 0)
           break;
         else
@@ -33,14 +33,46 @@ private:
     int maxSoFar{arr[0]};
 
     // loop over array
-    for (int i : arr) {
-      maxEndingHere = std::max(i, maxEndingHere * i, minEndingHere * i);
-      minEndingHere = std::min(i, maxEndingHere * i, minEndingHere * i);
+    for (int i{1}; i < arr.size(); ++i) {
+      // place holder to not cause overlap
+      int temp{};
+      temp = std::max({arr[i], maxEndingHere * arr[i], minEndingHere * arr[i]});
+      minEndingHere =
+          std::min({arr[i], maxEndingHere * arr[i], minEndingHere * arr[i]});
+      maxEndingHere = temp;
+      maxSoFar = std::max(maxSoFar, maxEndingHere);
     }
 
     return maxSoFar;
   }
+
   // Traversal approach
+  int traversal(std::array<int, 7> arr) {
+    int maxSoFar{arr[0]};
+    int product{1};
+
+    // iterate through array forwards
+    for (int i{0}; i < arr.size(); ++i) {
+      product *= arr[i];
+      if (arr[i] == 0)
+        product = 1;
+
+      maxSoFar = std::max(product, maxSoFar);
+    }
+
+    // reset value of product
+    product = 1;
+
+    // iterate through array backwards
+    for (int i{arr.size() - 1}; i >= 0; --i) {
+      product *= arr[i];
+      if (arr[i] == 0)
+        product = 1;
+      maxSoFar = std::max(product, maxSoFar);
+    }
+
+    return maxSoFar;
+  }
 
 public:
   // Prints array
@@ -54,6 +86,8 @@ public:
   // Returns results using all approaches
   void printSolution(const std::array<int, 7> &arr) {
     std::cout << "Approach A)" << nestedLoop(arr) << std::endl;
+    std::cout << "Approach B)" << kadane(arr) << std::endl;
+    std::cout << "Approach C)" << traversal(arr) << std::endl;
   }
 };
 
