3145c004fe29ab6a12ef5bf061be501cf44831a0
TimDTTP <timdttp@gmail.com>
2024-03-29 16:50:36 -0500

    Update: Added to logic
    - Unfinished add before leaving

--- leetcode/palindromeNumber/main.cpp
+++ leetcode/palindromeNumber/main.cpp
@@ -1,7 +1,37 @@
 
 #include <iostream>
+#include <stack>
 
 class Solution {
 public:
-  bool isPalindrome(int x) {}
+  bool isPalindrome(int x) {
+    // lorem ipsum
+    /*
+     * Approach
+     * - divide in half
+     * - push into stack
+     * - pop
+     *   - if equal, return true
+     *   - if not, return false
+     */
+    std::stack<int> start{};
+    std::stack<int> end{};
+
+    while (x > 0) {
+      int digit{x % 10};
+      x /= 10;
+      numStack.push(digit);
+    }
+
+    return true;
+  }
 };
+
+int main() {
+  int val{121};
+
+  Solution cursor = Solution();
+  cursor.isPalindrome(val);
+
+  return 0;
+}
