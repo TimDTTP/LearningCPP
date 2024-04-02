ad03b987f3f41ea22f15251f2e171c35d5f42875
TimDTTP <timdttp@gmail.com>
2024-04-02 13:50:34 -0500

    Update: Moved approach fcn into class
    - Moved nested loop approach into Solution class
    - Next is histogram approach

--- leetcode/longestCommonPrefix/main.cpp
+++ leetcode/longestCommonPrefix/main.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -15,38 +16,42 @@ bool isLesser(std::string &a, std::string &b) {
   return (a.length() < b.length());
 }
 
-// nested loop
-std::string approachA(std::vector<std::string> &strs) {
-  std::string result{""};
-
-  // sort vector from shortest word to longest
-  std::sort(strs.begin(), strs.end(), isLesser);
-
-  // loop #1 to loop through first word
-  for (int i{0}; i < strs[0].length(); ++i) {
-    // loop #2 to loop through vector
-    for (std::string j : strs) {
-      // exit condition
-      if (strs[0][i] != j[i]) {
-        return result;
-      }
-    }
-    result += strs[0][i];
-  }
-
-  return result;
-}
-
+// nested loop approach
 class SolutionA {
 public:
   std::string longestCommonPrefix(std::vector<std::string> &strs) {
-    return "true";
+    std::string result{""};
+
+    // sort vector from shortest word to longest
+    std::sort(strs.begin(), strs.end(), isLesser);
+
+    // loop #1 to loop through first word
+    for (int i{0}; i < strs[0].length(); ++i) {
+      // loop #2 to loop through vector
+      for (std::string j : strs) {
+        // exit condition
+        if (strs[0][i] != j[i]) {
+          return result;
+        }
+      }
+      result += strs[0][i];
+    }
+
+    return result;
   }
 };
 
+// map approach
+class SolutionB {
+public:
+  std::string longestCommonPrefix(std::vector<std::string> strs) {}
+}
+
 int main() {
   std::vector<std::string> sample{"fight", "figggg", "fign"};
-  std::cout << approachA(sample) << '\n';
+
+  SolutionA approachA;
+  std::cout << approachA.longestCommonPrefix(sample) << '\n';
 
   return 0;
 }
