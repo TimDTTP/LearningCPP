b1832b93626c9b38df8e69eb5d37ac0a070b6e0d
TimDTTP <timdttp@gmail.com>
2024-04-03 11:59:53 -0500

    Update: Finished approach
    - Stack approach finished
    - Looking to submit

--- leetcode/validParentheses/main.cpp
+++ leetcode/validParentheses/main.cpp
@@ -15,14 +15,40 @@ public:
 
     std::stack<char> open{};
 
+    // function to find key value
+    auto findKey{[](std::unordered_map<char, char> map, char lookUp) {
+      if (map.find(lookUp) == map.end())
+        return false;
+      else
+        return true;
+    }};
+
     // while loop
     for (char bracket : s) {
-      if (bracketPair.c {
+      if (findKey(bracketPair, bracket)) {
+        open.push(bracket);
+        continue;
+      } else if (bracketPair[open.top()] != bracket) {
+        return false;
+      } else if (bracketPair[open.top()] == bracket) {
+        open.pop();
       }
     }
-    // push into stack
-    // conditional
+
+    // check for dangling open parentheses
+    if (!open.empty())
+      return false;
+
+    return true;
   }
 };
 
-int main() { return 0; }
+int main() {
+  std::string s{"(){[]]}"};
+
+  Solution cursor = Solution();
+  std::cout << std::boolalpha;
+  std::cout << cursor.isValid(s) << '\n';
+
+  return 0;
+}
