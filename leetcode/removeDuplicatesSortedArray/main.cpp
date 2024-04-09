7b6cf25e8068d2a1a72cb219254ca0b83db0ac17
TimDTTP <timdttp@gmail.com>
2024-04-08 19:11:33 -0500

    Update: WIP logic

--- leetcode/removeDuplicatesSortedArray/main.cpp
+++ leetcode/removeDuplicatesSortedArray/main.cpp
@@ -7,14 +7,24 @@ class Solution {
 public:
   int removeDuplicates(std::vector<int> &nums) {
     // tracks index of array
-    int index{0};
+    int index{1};
     // tracks start of unique value
-    int start{0};
+    int start{1};
+
+    // edge case num.size() == 1
+    if (nums.size() == 1)
+      return 1;
+
+    // deal with first value
+    if (nums[0] == nums[index]) {
+      start = 0;
+    }
 
     while (index < nums.size()) {
-      if (nums[index] < nums[index + 1]) {
+      // FIX: Reverse checking
+      if (nums[index] > nums[index - 1]) {
         // delete
-        nums.erase(nums.begin() + (start + 1), nums.begin() + (index + 1));
+        nums.erase(nums.begin() + (start + 1), nums.begin() + index);
         // reset
         index = start;
         start = index + 1;
