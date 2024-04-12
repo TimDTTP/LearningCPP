21abf5b01ddbe69fb7a91c05a6db882ba4c8487d
TimDTTP <timdttp@gmail.com>
2024-04-12 12:42:36 -0500

    Update: WIP on approach

--- leetcode/removeKDigits/main.cpp
+++ leetcode/removeKDigits/main.cpp
@@ -1,56 +1,41 @@
 
 #include <iostream>
+#include <stack>
 #include <string>
 
 class Solution {
 public:
-  void deleteDigits(std::string &num, int start, int end) {
-    num.erase(num.begin() + start, num.begin() + end);
-  }
-
   std::string removeKdigits(std::string num, int k) {
-    int start{0};
-    int end{0};
+    std::stack<int> numStack{};
+    int length{static_cast<int>(num.length())};
+    int index{0};
 
-    // if length matches number of digit
-    if (num.length() == k)
-      return static_cast<std::string>("0");
-
-    // check if first k'th digits result in 0
-    if (num[k] == '0') {
-      bool found{false};
-      // find first non-leading zero integer
-      for (int i{k}; i < num.length(); ++i) {
-        if (num[i] != '0') {
-          found = true;
-          end = i;
-          break;
-        }
+    while (index < length && k > 0) {
+      if (numStack.empty()) {
+        numStack.push(num[index]);
+        continue;
       }
 
-      // if 0 is at the end of num
-      if (!found)
-        return static_cast<std::string>("0");
-
-      // delete k and leading 0's
-      deleteDigits(num, start, end);
-
-      return num;
+      if (numStack.top() < num[index])
+        numStack.push(num[index]);
+      else {
+        while (numStack.top() >= num[index] && k > 0) {
+          numStack.pop();
+          --k;
+        }
+        numStack.push(num[index]);
+      }
     }
 
-    // lastly, just delete largest value and following k'th digits
-    for (int digit{0}; digit <= (num.length() - k); ++digit) {
-      if (num[digit] > num[start])
-        start = digit;
-    }
-    deleteDigits(num, start, start + k);
+    // pull from stack and append to string
+    // remove leading 0's
 
     return num;
   }
 };
 
 int main() {
-  std::string num{"10"};
+  std::string num{"54321"};
   int target{1};
 
   Solution ans = Solution();
