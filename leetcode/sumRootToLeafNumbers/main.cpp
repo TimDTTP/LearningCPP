3bf874d4d53e3d9c77840901e1a34d5eceb75859
TimDTTP <timdttp@gmail.com>
2024-04-15 15:04:22 -0500

    Outline util functions needed to complete problems
    misc. function
    ie.
    - Create binary tree
    - print binary tree
    - test solution

--- leetcode/sumRootToLeafNumbers/main.cpp
+++ leetcode/sumRootToLeafNumbers/main.cpp
@@ -1,10 +1,25 @@
 
 #include "treeNode.h"
+#include <vector>
+
+// utils to help solve the problem
+class Util {
+public:
+  // vector into binary tree
+  // print tree
+  // test
+};
 
 class Solution {
 public:
-  int sumNumbers(TreeNode *root) {
-    // lorem
-    // ipsum
-  }
+  int sumNumbers(TreeNode *root) {}
 };
+
+int main() {
+  std::vector<std::vector<int>> tests{
+      {1, 2, 3},
+      {4, 9, 0, 5, 1},
+  };
+
+  return 0;
+}
