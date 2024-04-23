59d59e22690b5c7e1c4421723c814fc1b2e6768a
TimDTTP <timdttp@gmail.com>
2024-04-23 16:39:33 -0500

    Submitted

--- leetcode/longestPalindromicSubstring/main.cpp
+++ leetcode/longestPalindromicSubstring/main.cpp
@@ -5,11 +5,14 @@
 
 std::string test1() { return "ababa"; }
 std::string test2() { return "babad"; }
+std::string test3() { return "asdffds"; }
+std::string test4() { return "cbbd"; }
+std::string test5() { return "a"; }
 
 class Solution {
 private:
   std::string expandOdd(std::string s, int index) {
-    std::string largest{""};
+    std::string largest{};
     int maxDelta{};
     maxDelta = std::min(index, (static_cast<int>(s.size()) - index));
 
@@ -21,6 +24,33 @@ private:
         break;
     }
 
+    if (largest.empty())
+      largest = s[index];
+
+    return largest;
+  }
+
+  std::string expandEven(std::string s, int index) {
+    std::string largest{""};
+    int maxDelta{};
+
+    // check if next char is same, if NOT just return
+    if (s[index] != s[index + 1])
+      return largest;
+    else {
+      largest = s.substr(index, 2);
+    }
+
+    maxDelta = std::min(index, (static_cast<int>(s.size()) - index));
+
+    for (int delta{1}; delta <= maxDelta; ++delta) {
+      if (s[index - delta] == s[index + 1 + delta]) {
+        largest = s.substr(index - delta,
+                           ((index + 1 + delta) - (index - delta)) + 1);
+      } else
+        break;
+    }
+
     return largest;
   }
 
@@ -28,11 +58,21 @@ public:
   std::string longestPalindrome(std::string s) {
     std::string max{};
 
+    // call functions and compare to current largest thus far
+    for (int i{0}; i < s.size(); ++i) {
+      std::string even{expandEven(s, i)};
+      std::string odd{expandOdd(s, i)};
+
+      max = (max.size() > even.size()) ? max : even;
+      max = (max.size() > odd.size()) ? max : odd;
+    }
+
     return max;
   }
 
+  // TEST: REMOVE BEFORE SUBMITTING
   std::string testPrivate(std::string s, int index) {
-    std::string testStr{expandOdd(s, index)};
+    std::string testStr{expandEven(s, index)};
     return testStr;
   }
 };
@@ -40,9 +80,7 @@ public:
 int main() {
   Solution ans = Solution();
 
-  // std::cout << ans.longestPalindrome(test1()) << '\n';
-
-  std::cout << ans.testPrivate(test2(), 2) << '\n';
+  std::cout << ans.longestPalindrome(test2()) << '\n';
 
   return 0;
 }
