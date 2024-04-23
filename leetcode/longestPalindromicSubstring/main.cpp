0d5b50bcd1bb0b138e91893bd054cac1dac56b13
TimDTTP <timdttp@gmail.com>
2024-04-23 10:34:22 -0500

    Expand from middle (odd) completed

--- leetcode/longestPalindromicSubstring/main.cpp
+++ leetcode/longestPalindromicSubstring/main.cpp
@@ -4,16 +4,24 @@
 #include <string>
 
 std::string test1() { return "ababa"; }
+std::string test2() { return "babad"; }
 
 class Solution {
 private:
   std::string expandOdd(std::string s, int index) {
-    int delta{1};
-    std::string max{};
-    int max{std::min(index, s.size() - index)};
+    std::string largest{""};
+    int maxDelta{};
+    maxDelta = std::min(index, (static_cast<int>(s.size()) - index));
 
-    if (s[index - delta] == s[index + delta]) {
+    for (int delta{1}; delta <= maxDelta; ++delta) {
+      if (s[index - delta] == s[index + delta]) {
+        largest =
+            s.substr(index - delta, ((index + delta) - (index - delta)) + 1);
+      } else
+        break;
     }
+
+    return largest;
   }
 
 public:
@@ -22,12 +30,19 @@ public:
 
     return max;
   }
+
+  std::string testPrivate(std::string s, int index) {
+    std::string testStr{expandOdd(s, index)};
+    return testStr;
+  }
 };
 
 int main() {
   Solution ans = Solution();
 
-  std::cout << ans.longestPalindrome(test1()) << '\n';
+  // std::cout << ans.longestPalindrome(test1()) << '\n';
+
+  std::cout << ans.testPrivate(test2(), 2) << '\n';
 
   return 0;
 }
