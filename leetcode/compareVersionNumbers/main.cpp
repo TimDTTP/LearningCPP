d9a30f2cb13c3fecfabeab0a7bcd964df1f3b8a3
TimDTTP <timdttp@gmail.com>
2024-05-03 02:50:30 -0500

    Passed: initial test passed

--- leetcode/compareVersionNumbers/main.cpp
+++ leetcode/compareVersionNumbers/main.cpp
@@ -35,12 +35,83 @@ TestCase test3() {
   return obj;
 }
 
+TestCase test4() {
+  TestCase obj;
+  obj.version1 = "1.0.1";
+  obj.version2 = "1";
+  obj.expected = 1;
+
+  return obj;
+}
+
+TestCase test5() {
+  TestCase obj;
+  obj.version1 =
+      "19.8.3.17.5.01.0.0.4.0.0.0.0.0.0.0.0.0.0.0.0.0.00.0.0.0.0.0.0.0.0.0.0.0."
+      "0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0."
+      "0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0."
+      "0.0.0.0.0.0.0.0.0.000000.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0."
+      "0.0.0.0.0.0.0.0.0.0.0.000000";
+  obj.version2 =
+      "19.8.3.17.5.01.0.0.4.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0."
+      "0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0."
+      "0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0."
+      "0.0.0.0.0.0.0.0000.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0."
+      "0.0.0.0.0.0.0.0.0.0.0.0.000000";
+  obj.expected = 1;
+
+  return obj;
+}
+
 class Solution {
 public:
-  int compareVersion(std::string version1, std::string version2) { return 0; }
+  int compareVersion(std::string version1, std::string version2) {
+    size_t v1{version1.size()};
+    size_t v2{version2.size()};
+    int ptr1{0};
+    int ptr2{0};
+
+    while (ptr1 < v1 || ptr2 < v2) {
+      int val1{0};
+      int val2{0};
+
+      // value up to decimal
+      if (ptr1 < v1) {
+        while ((version1[ptr1] != '.') && (ptr1 < v1)) {
+          val1 = val1 * 10 + (version1[ptr1] - '0');
+          ++ptr1;
+        }
+      }
+      if (ptr2 < v2) {
+        while ((version2[ptr2] != '.') && (ptr2 < v2)) {
+          val2 = val2 * 10 + (version2[ptr2] - '0');
+          ++ptr2;
+        }
+      }
+
+      // compare
+      if (val1 < val2)
+        return -1;
+      else if (val1 > val2)
+        return 1;
+
+      // skip decimal
+      ++ptr1;
+      ++ptr2;
+    }
+
+    return 0;
+  }
 };
 
 int main() {
-  TestCase input{test1()};
+  TestCase input{test5()};
   Solution answer = Solution();
+
+  int res{answer.compareVersion(input.version1, input.version2)};
+
+  std::cout << "Expected: " << '\n' << input.expected << "\n\n";
+  std::cout << "Actual: " << '\n' << res << "\n\n";
+
+  return 0;
 }
