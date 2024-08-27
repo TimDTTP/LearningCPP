4fe5892aa927bb10ab26878d5b53540883cc04aa
TimDTTP <timdttp@gmail.com>
2024-08-27 12:16:12 -0500

    Update: added aux functions
    Added following functions:
    Vector -> tree
    Print tree

--- leetcode/treePostOrderTraversal/main.cpp
+++ leetcode/treePostOrderTraversal/main.cpp
@@ -5,7 +5,6 @@
 
 class Test {
 private:
-  // FIX: root is not const and changing per iteration
   // convert vector to tree
   Node *tree(std::vector<const int> values) {
     Node *root = nullptr;
@@ -19,11 +18,11 @@ private:
     // loop and add till -1
     for (int nums{0}; nums < values.size(); ++nums) {
       // Init value node
-      Node temp{Node(values[nums])};
+      Node *temp{new Node(values[nums])};
 
       // handle root node
       if (root == nullptr) {
-        root = &temp;
+        root = temp;
         order.push(root);
 
         nums += 1;
@@ -34,39 +33,67 @@ private:
 
         continue;
       }
+
       // if -1
-      if (values[nums] == -1) {
+      if (values[nums] == -1 || nums == values.size()) {
         order.front()->children = tempChildren;
-        tempChildren.clear();
         order.pop();
+
+        // add children to parent queue
+        for (Node *i : tempChildren) {
+          order.push(i);
+        }
+
+        tempChildren.clear();
       }
       // handle actual ideal case
       else {
-        tempChildren.push_back(&temp);
+        tempChildren.push_back(temp);
       }
     }
+
+    // add last group of children cause no ending NULL
+    order.front()->children = tempChildren;
 
     return root;
   }
 
   // print testcase
   void printTree(Node *root) {
-    // std::cout << root->val << '\n';
-    // std::queue<Node *> printOrder;
-    // printOrder.push(root);
-    //
-    // while (!printOrder.empty()) {
-    //   for (int i{0}; i < (printOrder.front()->children.size()); ++i) {
-    //     printOrder.push(printOrder.front()->children[i]);
-    //   }
-    //   std::cout << printOrder.front() << ' ';
-    //   printOrder.pop();
-    // }
+    std::vector<Node *> parent;
+    std::vector<Node *> children;
+
+    parent.push_back(root);
+
+    while (!parent.empty()) {
+      for (Node *i : parent.front()->children) {
+        children.push_back(i);
+      }
+
+      std::cout << parent.front()->val << ' ';
+      parent.erase(parent.begin());
+
+      if (parent.empty() && !children.empty()) {
+        std::cout << '\n';
+        parent = children;
+        children.clear();
+      }
+    }
   }
 
 public:
   Node *testA() {
     std::vector<const int> obj{1, -1, 3, 2, 4, -1, 5, 6};
+
+    Node *objTree{tree(obj)};
+    printTree(objTree);
+
+    return objTree;
+  }
+
+  Node *testB() {
+    std::vector<const int> obj{1, -1, 2,  3,  4,  5,  -1, -1, 6,  7,  -1, 8, -1,
+                               9, 10, -1, -1, 11, -1, 12, -1, 13, -1, -1, 14};
 
     Node *objTree{tree(obj)};
     printTree(objTree);
@@ -86,7 +113,7 @@ public:
 
 int main() {
   Test testCur = Test();
-  Node *caseA{testCur.testA()};
+  Node *caseA{testCur.testB()};
 
   return 0;
 }
