264a1e2e908bdff84955b65e2990325c8ae18081
TimDTTP <timdttp@gmail.com>
2024-09-16 16:02:21 -0500

    Update: passed preliminary tests

--- leetcode/minimumTimeDifference/main.cpp
+++ leetcode/minimumTimeDifference/main.cpp
@@ -1,8 +1,90 @@
 
+#include <iostream>
 #include <string>
 #include <vector>
 
-class Solution {
+class Tests {
 public:
-  int findMinDifference(std::vector<std::string> &timePoints) {}
+  struct testCase {
+    std::vector<std::string> input;
+    int output;
+  };
+
+  testCase testA() {
+    testCase obj;
+    obj.input = {"23:59", "00:00"};
+    obj.output = 1;
+
+    return obj;
+  }
+
+  testCase testB() {
+    testCase obj;
+    obj.input = {"00:00", "23:59", "00:00"};
+    obj.output = 0;
+
+    return obj;
+  }
 };
+
+class Solution {
+private:
+  const int minInHour{60};
+  const int hourInDay{24};
+
+  int shortestPath(int base, int cmp) {
+    int greater{std::max(base, cmp)};
+    int lesser{std::min(base, cmp)};
+
+    int path{greater - lesser};
+
+    if (path > (minInHour * (hourInDay / 2))) {
+      path = (((hourInDay * minInHour) - greater) + lesser);
+    }
+
+    return path;
+  }
+
+public:
+  int findMinDifference(std::vector<std::string> &timePoints) {
+
+    // convert time points into flat integers
+    int hours, minutes;
+    std::vector<int> pointInMins;
+    int temp;
+    for (std::string time : timePoints) {
+      hours = std::stoi(time.substr(0, 2));
+      minutes = std::stoi(time.substr(3, 5));
+      temp = (hours * minInHour) + minutes;
+      pointInMins.push_back(temp);
+    }
+
+    int minPath{hourInDay * minInHour};
+    for (int base{0}; base < timePoints.size(); ++base) {
+      for (int comp{base + 1}; comp < timePoints.size(); ++comp) {
+        minPath = std::min(minPath,
+                           shortestPath(pointInMins[base], pointInMins[comp]));
+      }
+    }
+
+    return minPath;
+  }
+};
+
+int main() {
+  Tests testCur = Tests();
+  Solution solCur = Solution();
+
+  Tests::testCase test{testCur.testB()};
+
+  int output{solCur.findMinDifference(test.input)};
+
+  if (output == test.output)
+    std::cout << "Success!\n" << std::endl;
+  else {
+    std::cout << "Fail!\n";
+    std::cout << output << '\n' << std::endl;
+  }
+
+  return 0;
+}
