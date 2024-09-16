31047104558dfdc74770ba78a7cc7bc2e548afdf
TimDTTP <timdttp@gmail.com>
2024-09-16 14:54:26 -0500

    Update: submitted and passed
    runtime 0ms beats 100%
    memory 7.46mB beats 92.77%

--- leetcode/minimumBitflipToConvertNumber/main.cpp
+++ leetcode/minimumBitflipToConvertNumber/main.cpp
@@ -1,5 +1,31 @@
 
+#include <bitset>
+#include <iostream>
+#include <string>
+
 class Solution {
 public:
-  int minBitFlips(int start, int goal) {}
+  int minBitFlips(int start, int goal) {
+    std::bitset<32> bitStart{static_cast<unsigned>(start)};
+    std::bitset<32> bitGoal{static_cast<unsigned>(goal)};
+
+    // compare start vs goal
+    int counter{0};
+    for (int i{0}; i < 32; ++i) {
+      if (bitStart[i] != bitGoal[i])
+        ++counter;
+    }
+
+    return counter;
+  }
 };
+
+int main() {
+  std::cout << "Start\n" << std::endl;
+
+  Solution cursor = Solution();
+  int output = cursor.minBitFlips(3, 4);
+  std::cout << output << '\n';
+
+  return 0;
+}
