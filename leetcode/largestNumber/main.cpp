b7aa3b951db10d64f69ce1109c33031b670e2e8c
TimDTTP <timdttp@gmail.com>
2024-09-18 13:57:31 -0500

    Update: finished application of approach w/o testing yet

--- leetcode/largestNumber/main.cpp
+++ leetcode/largestNumber/main.cpp
@@ -1,8 +1,39 @@
 
+#include <algorithm>
 #include <string>
 #include <vector>
 
 class Solution {
+private:
+  bool static cmp(std::pair<std::string, int> first,
+                  std::pair<std::string, int> second) {
+    if (first.second > second.second)
+      return true;
+    else
+      return false;
+  };
+
 public:
-  std::string largestNumber(std::vector<int> &nums) {}
+  std::string largestNumber(std::vector<int> &nums) {
+    std::string out{""};
+    std::vector<std::pair<std::string, int>> table;
+
+    // add value : priority to table
+    for (int num : nums) {
+      std::pair<std::string, int> obj;
+      obj.first = std::to_string(num);
+      obj.second = num * (pow(10, (9 - obj.first.size() - 1)));
+      table.push_back(obj);
+    }
+
+    // sort table by priority
+    std::sort(table.begin(), table.end(), cmp);
+
+    // iterate and append to string
+    for (std::pair<std::string, int> vals : table) {
+      out += vals.first;
+    }
+
+    return out;
+  }
 };
