8e51d6847064d5189b234ba0a56f49356a0bdb33
TimDTTP <timdttp@gmail.com>
2024-10-02 15:22:56 -0500

    Update: Unable to finish in time
    will revisit another time

--- leetcode/findLengthOfLongestCommonPrefix/main.cpp
+++ leetcode/findLengthOfLongestCommonPrefix/main.cpp
@@ -33,28 +33,121 @@ class Solution {
 private:
   struct Node {
     int val;
-    std::vector<Node *> subsequent;
+    std::vector<Node *> children;
+
+    Node() { val = 0; }
+    Node(int num) { val = num; }
   };
 
   Node *initTree(std::vector<int> arr) {
-    Node head;
-    head.val = 0;
+    Node *head = new Node();
+    head->val = 0;
+    Node *tail{head};
+    Node *trav{head};
+    std::stack<int> digits;
 
     for (int i : arr) {
+      trav = head;
       int temp{i};
+
       while (i > 0) {
         temp = i % 10;
+        digits.push(temp);
         i /= 10;
+      }
 
-        // find if value exists
+      // iterate through tree and check
+      while (!digits.empty()) {
+        // iterate through children and find digit
+        for (Node *i : trav->children) {
+          if (i->val == digits.top()) {
+            tail = i;
+            break;
+          }
+        }
+
+        // if does not exist, add
+        if (head == tail) {
+          Node *newNode = new Node(digits.top());
+          trav->children.push_back(newNode);
+          trav = newNode;
+        }
+        // if does exist skip
+        else {
+          trav = tail;
+        }
+
+        digits.pop();
+      }
+    }
+
+    return head;
+  }
+
+  int lengthOfMatch(Node *const tree, int num) {
+    int counter{0};
+    Node *trav{tree};
+    std::stack<int> digits;
+    int temp;
+    bool flag{false};
+
+    // get each digits in descending order
+    while (num > 0) {
+      temp = (num % 10);
+      num /= 10;
+      digits.push(temp);
+    }
+
+    // check against tree, exit early if pre-existing
+    while (!digits.empty()) {
+      flag = false;
+
+      for (Node *i : trav->children) {
+        if (i->val == digits.top()) {
+          flag = true;
+          ++counter;
+          trav = i;
+          continue;
+        }
+      }
+
+      if (!flag)
+        return counter;
+    }
+
+    return counter;
+  }
+
+public:
+  void printTree(Node *root) {
+    std::queue<Node *> order;
+    int up{0};
+    int down{1};
+    order.push(root);
+
+    while (!order.empty()) {
+      for (Node *i : order.front()->children) {
+        ++up;
+        order.push(i);
+      }
+      std::cout << order.front()->val << ' ';
+      order.pop();
+      --down;
+
+      if (down == 0) {
+        std::cout << std::endl;
+        down = up;
+        up = 0;
       }
     }
   }
 
-public:
   int longestCommonPrefix(std::vector<int> &arr1, std::vector<int> &arr2) {
     int maxPrefix;
     // add values of arr2 to tree
+    Node *arr2Tree{initTree(arr2)};
+
+    maxPrefix = std::max();
 
     return maxPrefix;
   }
