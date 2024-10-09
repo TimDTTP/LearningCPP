4131bd753d011d475940ab83b39a2f8c37033e98
TimDTTP <timdttp@gmail.com>
2024-10-09 13:37:05 -0500

    Update: passed initial tests

--- leetcode/minimumAddToMakeParenthesesValid/main.cpp
+++ leetcode/minimumAddToMakeParenthesesValid/main.cpp
@@ -1,7 +1,38 @@
 
+#include <iostream>
+#include <sstream>
 #include <string>
 
 class Solution {
 public:
-  int minAddToMakeValid(std::string s) {}
+  int minAddToMakeValid(std::string s) {
+    std::stringstream ss{s};
+    char c;
+    std::stack<char> currParentheses;
+    int counter;
+
+    while (ss >> c) {
+      if (c == '(')
+        currParentheses.push(c);
+      else {
+        if (currParentheses.empty())
+          counter++;
+        else
+          currParentheses.pop();
+      }
+    }
+
+    // if remaining '('
+    if (!currParentheses.empty())
+      counter += currParentheses.size();
+
+    return counter;
+  }
 };
+
+int main() {
+  Solution solCur = Solution();
+
+  int output{solCur.minAddToMakeValid("(((")};
+  std::cout << output << '\n';
+}
