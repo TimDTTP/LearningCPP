214bcedf84957759d052b532326389fee3027cf1
TimDTTP <timdttp@gmail.com>
2024-10-20 10:00:25 -0500

    WIP: approach does not fulfill correct conditions

--- leetcode/longestHappyString/main.cpp
+++ leetcode/longestHappyString/main.cpp
@@ -1,7 +1,8 @@
 
+#include <algorithm>
 #include <iostream>
-#include <map>
 #include <string>
+#include <vector>
 
 class Test {
 public:
@@ -29,9 +30,9 @@ public:
 
   TestCase testC() {
     TestCase obj;
-    obj.a = 76;
-    obj.b = 69;
-    obj.c = 92;
+    obj.a = 2;
+    obj.b = 2;
+    obj.c = 1;
     return obj;
   }
 };
@@ -41,8 +42,6 @@ private:
   bool static cmp(std::pair<char, int> &a, std::pair<char, int> &b) {
     return a.second > b.second;
   }
-
-  int numPos(int a) { return ((a / 2) + (a % 2)); }
 
 public:
   std::string longestDiverseString(int a, int b, int c) {
@@ -58,12 +57,62 @@ public:
     }
     std::cout << std::endl;
 
-    int maxPos{numPos(table[0].second)};
+    // to double up or not
+    bool doubleUp{table[0].second > (table[1].second + table[2].second)};
+    std::string output{""};
 
-    if (table[1].second < maxPos) {
+    if (!doubleUp) {
+      int xz{table[0].second - table[1].second};
+      int xyz{table[2].second - xz};
+
+      // string creation
+      for (int i{0}; i < (xyz + xz); i++) {
+        if (xyz > 0) {
+          output += table[0].first;
+          output += table[1].first;
+          output += table[2].first;
+          xyz--;
+        } else {
+          output += table[0].first;
+          output += table[2].first;
+          xz--;
+        }
+      }
+
+      return output;
+    } else {
+      std::cout << "here" << '\n';
+      int doubleLargest{};
+      int maxPosition{table[1].second + table[2].second + 1};
+      if (table[0].second * 2 > maxPosition)
+        table[0].second = maxPosition * 2;
+
+      doubleLargest = table[0].second - maxPosition;
+
+      // string creation
+      while (table[0].second > 0 || table[1].second > 0 ||
+             table[2].second > 0) {
+        if (doubleLargest > 0) {
+          output += table[0].first;
+          output += table[0].first;
+          doubleLargest--;
+          table[0].second -= 2;
+        } else {
+          output += table[0].first;
+        }
+        if (table[1].second > 0) {
+          output += table[1].first;
+          table[1].second--;
+        } else if (table[2].second > 0) {
+          output += table[2].first;
+          table[2].second--;
+        }
+      }
+
+      return output;
     }
 
-    return "SAMPLE";
+    return output;
   }
 };
 
@@ -71,7 +120,7 @@ int main() {
   Test testCur = Test();
   Solution solCur = Solution();
 
-  Test::TestCase input{testCur.testA()};
+  Test::TestCase input{testCur.testC()};
   std::string output{solCur.longestDiverseString(input.a, input.b, input.c)};
   std::cout << output << std::endl;
 
