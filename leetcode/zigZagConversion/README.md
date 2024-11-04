9c4f2748e6fcc54a05c624a2dac1b1daff301e33
TimDTTP <timdttp@gmail.com>
2024-11-04 13:00:14 -0600

    finished
    Runtime 0ms Beats 100%
    Memory 10.83mB Beats 80.22%

--- leetcode/zigZagConversion/README.md
+++ leetcode/zigZagConversion/README.md
@@ -19,3 +19,13 @@ converting the string into a zig zag patter with "numRows" number of rows.
 - 1 <= numRows <= 1000
 
 ## Approach
+- If "numRows" is 1, return 's'
+- For "numRows" to get each of first 5 values
+    - DB is distance from bottom \(row - 0)
+    - DT is distance from top \(numRows - row)
+    - ALternate between formula DB * 2 and DT * 2
+    - Top and bottom most row is \(numRows - 1) * 2
+- Append until index is out of scope
+
+Runtime 0ms Beats 100%
+Memory 10.83mB Beats 80.22%
--- leetcode/zigZagConversion/main.cpp
+++ leetcode/zigZagConversion/main.cpp
@@ -1,4 +1,5 @@
 
+#include <cstddef>
 #include <iostream>
 
 class Test {
@@ -36,14 +37,58 @@ public:
 
 class Solution {
 public:
-  std::string convert(std::string s, int numRows) {}
+  std::string convert(std::string s, int numRows) {
+    std::size_t size{s.size()};
+    std::string convStr{""};
+
+    // edge case
+    if (numRows == 1)
+      return s;
+
+    // interate through first numRows
+    int temp;
+    bool down;
+    for (int i{0}; i < numRows; i++) {
+      if (i >= size)
+        return convStr;
+      convStr.push_back(s[i]);
+      temp = i;
+      down = true;
+      if (i == 0 || i == numRows - 1) {
+        temp += (numRows - 1) * 2;
+        while (temp < size) {
+          convStr.push_back(s[temp]);
+          temp += (numRows - 1) * 2;
+        }
+      } else {
+        temp += (numRows - (i + 1)) * 2;
+        while (temp < size) {
+          // down direction
+          if (down) {
+            convStr.push_back(s[temp]);
+            temp += i * 2;
+            down = false;
+          }
+
+          // up direction
+          else {
+            convStr.push_back(s[temp]);
+            temp += (numRows - (i + 1)) * 2;
+            down = true;
+          }
+        }
+      }
+    }
+
+    return convStr;
+  }
 };
 
 int main() {
   Test testCur{Test()};
   Solution solCur{Solution()};
 
-  Test::TestCase unit{testCur.testA()};
+  Test::TestCase unit{testCur.testC()};
   std::string output{solCur.convert(unit.s, unit.numRows)};
 
   if (output == unit.output)
