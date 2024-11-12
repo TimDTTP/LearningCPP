35e1dfb4f4b08accac5b94ecac5b19fdddbc78fa
TimDTTP <timdttp@gmail.com>
2024-11-12 13:54:34 -0600

    Submitted and passed
    Runtime 0ms Beats 100%
    Memory 27.46mB Beats 67.12%

--- leetcode/primeSubtractionOperation/README.md
+++ leetcode/primeSubtractionOperation/README.md
@@ -9,8 +9,8 @@ Return...
 Where...
 the return value is if you are able to convert "nums" to a strictly increasing 
 array. Conversions must follow the following...
-- At any index, 'i', (that hasn't been picked before) pick a prime number, 'p', 
-strictly less than nums\[i], then subtract 'p' from nums\[i]
+- At any index, 'i', (that hasn't been picked before) pick a prime number, 
+'p', strictly less than nums\[i], then subtract 'p' from nums\[i]
 - This can be done any amount of times
 
 [#2601 Prime Subtraction Operation](https://leetcode.com/problems/prime-subtraction-operation/description/)
@@ -21,4 +21,13 @@ strictly less than nums\[i], then subtract 'p' from nums\[i]
 - nums.length == n
 
 ## Approach
+- Traverse array backwards till found a value is that is not descending
+- From there, find a prime value to subtract by that will be less than 
+previous value.
+- Use a separate array to use as memoization to track found prime numbers
+- If not already in prime array, traverse until another prime number is found
+- Exit conditions:
+    - No prime number below num\[i] that will be less than num\[i + 1]
 
+Runtime 0ms Beats 100%
+Memory 27.46mB Beats 67.12%
--- leetcode/primeSubtractionOperation/main.cpp
+++ leetcode/primeSubtractionOperation/main.cpp
@@ -26,15 +26,73 @@ public:
 };
 
 class Solution {
+private:
+  bool isPrime(int num, const std::vector<int> &primes) {
+    int perfectSquare = std::sqrt(num);
+
+    // find all prime under perfect square if divisible
+    int index{0};
+    while (primes[index] <= perfectSquare) {
+      if (num % primes[index] == 0)
+        return false;
+
+      index++;
+    }
+
+    return true;
+  }
+
 public:
-  bool primeSubOperation(std::vector<int> &nums) {}
+  bool primeSubOperation(std::vector<int> &nums) {
+    int n = nums.size();
+    std::vector<int> primes{2, 3};
+
+    // loop through vector for pattern
+    for (int i{n - 2}; i >= 0; i--) {
+      // not in descending order (backwards)
+      if (nums[i] >= nums[i + 1]) {
+        // find prime value
+        // if already in array
+        if (nums[i] - primes[primes.size() - 1] < nums[i + 1]) {
+          for (int prime : primes) {
+            if (prime >= nums[i])
+              return false;
+            else if (nums[i] - prime < nums[i + 1]) {
+              nums[i] -= prime;
+              break;
+            }
+          }
+        }
+
+        // not in array, have to find
+        else {
+          for (int j{primes[primes.size() - 1] + 1}; j < nums[i]; j++) {
+            // check if prime
+            if (isPrime(j, primes)) {
+              primes.push_back(j);
+              if (nums[i] - j < nums[i + 1]) {
+                nums[i] -= j;
+                break;
+              }
+            }
+          }
+
+          if (nums[i] - primes[primes.size() - 1] >= nums[i + 1]) {
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }
 };
 
 int main() {
   Test testCur{Test()};
   Solution solCur{Solution()};
 
-  Test::TestCase unit{testCur.testA()};
+  Test::TestCase unit{testCur.testC()};
   bool output{solCur.primeSubOperation(unit.nums)};
 
   std::cout << std::boolalpha;
