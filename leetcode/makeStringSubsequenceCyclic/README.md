f01104fbad4a58835a717932c4417111e8403cdf
TimDTTP <timdttp@gmail.com>
2024-12-04 11:13:50 -0600

    finished: solved and reviewed
    Runtime: 2ms Beats 72.38%
    Memory: 18.43mB Beats 7.53%

--- leetcode/makeStringSubsequenceCyclic/README.md
+++ leetcode/makeStringSubsequenceCyclic/README.md
@@ -21,3 +21,16 @@ the increment once. Return if it is possible to make `str2` a subsequence of
 - str1 and str2 consist of only lowercase English letters
 
 ## Approach
+- Instantiate a third string, incremented `str1`
+- Increment through `str2`, if `str2\[i]` is in `str1` or `str2`
+    - Increment `str2`
+    - If `str2` is out of bounds, return true
+- Increment `str1` and `str2`
+    - If `str1` is out of bounds, return false
+
+Runtime: 2ms Beats 72.38%
+Memory: 18.43mB Beats 7.53%
+
+## Online Solution
+- No need to instantiate third string for incremented string. Better to 
+manipulate character's unicode values to compare distance and match.
--- leetcode/makeStringSubsequenceCyclic/main.cpp
+++ leetcode/makeStringSubsequenceCyclic/main.cpp
@@ -18,9 +18,54 @@ public:
 
 class Solution {
 public:
-  bool canMakeSubsequnce(std::string str1, std::string str2) {
-    bool out;
-    return out;
+  bool canMakeSubsequence(std::string str1, std::string str2) {
+    const int size1 = str1.size();
+    const int size2 = str2.size();
+
+    // str1 cannot be smaller than str2 in order to be true
+    if (size1 < size2)
+      return false;
+
+    // create a separate string of str1 that has all incremented
+    std::string inc{str1};
+    for (int i{0}; i < size1; i++) {
+      inc[i] = char(inc[i] + 1);
+      // cyclical property
+      if (inc[i] == '{')
+        inc[i] = 'a';
+    }
+
+    // increment through and compare str2 to str1/inc to find matches
+    int ptr1{0}, ptr2{0};
+    for (int i{0}; i < size1; i++) {
+      if (str2[ptr2] == str1[ptr1] || str2[ptr2] == inc[ptr1]) {
+        ++ptr2;
+        if (ptr2 == size2)
+          return true;
+      }
+      ++ptr1;
+
+      // too many chars that do not line up thus impossible to be true
+      if ((ptr1 - ptr2) > (size1 - size2))
+        return false;
+    }
+
+    return false;
+  }
+};
+
+class OnlineSolution {
+public:
+  bool canMakeSubsequence(std::string source, std::string target) {
+    int targetIdx = 0;
+    int targetLen = target.length();
+    for (char currChar : source) {
+      if (targetIdx < targetLen &&
+          (target[targetIdx] - currChar + 26) % 26 <= 1) {
+        targetIdx++;
+      }
+    }
+    return targetIdx == targetLen;
   }
 };
 
@@ -29,8 +74,9 @@ int main() {
   Solution solCur{Solution()};
 
   Test::TestCase unit{testCur.testA()};
-  bool output{solCur.canMakeSubsequnce(unit.str1, unit.str2)};
+  bool output{solCur.canMakeSubsequence(unit.str1, unit.str2)};
 
+  std::cout << std::boolalpha;
   if (output == unit.output)
     std::cout << "Success!\n";
   else {
