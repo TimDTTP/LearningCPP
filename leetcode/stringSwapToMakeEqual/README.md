e6867f452ee2ad3dd537804898befb379caadadd
TimDTTP <timdttp@gmail.com>
2025-02-05 12:48:22 -0600

    init: new problem
    #1790 Check if one string swap can make strings equal

---
+++ leetcode/stringSwapToMakeEqual/README.md
@@ -0,0 +1,25 @@
+
+# Problem
+Given...
+- A string, s1
+- A string, s2
+
+Return...
+- A bool
+
+Where...
+it is possible to perform at most one string swap on exactly one string to make
+both s1 and s2 equal to each other.
+- A string swap is an operation where you can choose 2 indices of a string to
+swap the characters
+
+[#1790 Check if one string swap can make strings equal](https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/description/?envType=daily-question&envId=2025-02-05)
+
+## Constraints
+- 1 <= s1.length, s2.length <= 100
+- s1.length == s2.length
+- s1 and s2 consist of only lowercase English letters
+
+## Approach
+
+
---
+++ leetcode/stringSwapToMakeEqual/main.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <string>
+
+class Test {
+public:
+  struct TestCase {
+    std::string s1;
+    std::string s2;
+    bool output;
+  };
+
+  TestCase testA() {
+    TestCase obj;
+    obj.s1 = " ";
+    return obj;
+  }
+};
+
+class Solution {
+public:
+  bool areAlmostEqual(std::string s1, std::string s2) {
+    bool out;
+    return out;
+  }
+};
+
+int main() {
+  Test testCur{Test()};
+  Solution solCur{Solution()};
+
+  Test::TestCase unit{testCur.testA()};
+  bool output{solCur.areAlmostEqual(unit.s1, unit.s2)};
+
+  if (output == unit.output)
+    std::cout << "Success!\n";
+  else {
+    std::cout << "Failed!\n";
+    std::cout << "Expected: " << unit.output << '\n';
+    std::cout << "Actual: " << output << '\n';
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
