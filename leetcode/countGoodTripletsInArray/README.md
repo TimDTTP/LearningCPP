b420b02d71509f7832688f3ca209be7bc9316623
TimDTTP <timdttp@gmail.com>
2025-04-15 16:41:18 -0500

    failed: passed alotted time limit

--- leetcode/countGoodTripletsInArray/README.md
+++ leetcode/countGoodTripletsInArray/README.md
@@ -15,10 +15,24 @@ order in both vectors.
 [#2179 Count Good Triplets in an Array](https://leetcode.com/problems/count-good-triplets-in-an-array/description/?envType=daily-question&envId=2025-04-15)
 
 ## Constraint
-
-- n == nums1.length == nums2.length
+- n = nums1.length = nums2.length
 - 3 <= n <= 10^5
 - 0 <= nums1\[i], nums2\[i] <= n - 1
 
 ## Approach
+Brute force
+- For each vector, add every permutation into a set
+- Compare each set and count number of similarities
+
+Map lookup
+- Add all values in nums2 to a map value:index
+- Loop through nums1 until n - 3
+- Go through every permutation of nums1
+- Check if values in nums2 match requirements
+- Count number that do
+
+Note:
+Seemingly works upto testcase 102/143. Hits error "Time Limit Exceeded".
+
+Failed: Surpassed alotted time
 
--- leetcode/countGoodTripletsInArray/main.cpp
+++ leetcode/countGoodTripletsInArray/main.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 class Test {
 public:
@@ -24,13 +26,46 @@ public:
     obj.output = 4;
     return obj;
   }
+
+  TestCase testC() {
+    TestCase obj;
+    obj.nums1 = {13, 14, 10, 2, 12, 3, 9, 11, 15, 8, 4, 7, 0, 6, 5, 1};
+    obj.nums2 = {8, 7, 9, 5, 6, 14, 15, 10, 2, 11, 4, 13, 3, 12, 1, 0};
+    obj.output = 77;
+    return obj;
+  }
 };
 
 class Solution {
 public:
   long long goodTriplets(std::vector<int> &nums1, std::vector<int> &nums2) {
-    long long out;
-    return out;
+    long long counter{0};
+
+    // add nums2 to a map for better lookup
+    const size_t size{nums2.size()};
+    std::unordered_map<int, int> screen;
+    for (int i{0}; i < size; ++i) {
+      screen.insert({nums2[i], i});
+    }
+
+    // loop through nums1 and find permutation
+    for (int first{0}; first < (size - 2); ++first) {
+      for (int second{first + 1}; second < (size - 1); ++second) {
+        // check second value is after first
+        if (screen[nums1[first]] < screen[nums1[second]]) {
+          for (int third{second + 1}; third < size; ++third) {
+            // check third value is after second
+            if (screen[nums1[second]] < screen[nums1[third]]) {
+              ++counter;
+            }
+          }
+        } else {
+          continue;
+        }
+      }
+    }
+
+    return counter;
   }
 };
 
@@ -38,7 +73,7 @@ int main() {
   Test testCur{Test()};
   Solution solCur{Solution()};
 
-  Test::TestCase unit{testCur.testA()};
+  Test::TestCase unit{testCur.testB()};
   long long output{solCur.goodTriplets(unit.nums1, unit.nums2)};
 
   if (output == unit.output)
