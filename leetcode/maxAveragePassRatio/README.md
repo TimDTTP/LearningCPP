0602b236f914e80349d0ad4f002e9df6729276f3
TimDTTP <timdttp@gmail.com>
2025-09-01 20:00:48 -0500

    init

---
+++ leetcode/maxAveragePassRatio/README.md
@@ -0,0 +1,32 @@
+
+# Problem
+Given...
+    - A vector\<vector\<int>>, classes
+    - A int, extraStudents
+
+Return...
+    - A double
+
+Where...
+    - The return value is the maximum possible average pass ratio after 
+assigning the "extraStudents".
+    - classes\[i] = \[pass, total]
+        - \[Total number of passing grades, total number of students in class i]
+    - extraStudents are guaranteed to pass the exam of any class they are 
+assigned to
+    - Pass ratio = \# of passing grades \/ number of students in class i
+    - Average pass ratio = sum of pass ratios \/ number of classes
+
+[\#1792 Maximum Average Pass Ratio](https://leetcode.com/problems/maximum-average-pass-ratio/description/)
+
+## Constraint
+- 1 <= classes.length <= 10^5
+- classes\[i].length == 2
+- 1 <= pass <= total <= 10^5
+- 1 <= extraStudents <= 10^5
+
+## Approach
+-
+
+## Improvements
+
---
+++ leetcode/maxAveragePassRatio/main.cpp
@@ -0,0 +1,109 @@
+
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <utility>
+#include <vector>
+
+class Test {
+public:
+  struct TestCase {
+    std::vector<std::vector<int>> classes;
+    int extraStudents;
+    double output;
+  };
+
+  TestCase testA() {
+    TestCase obj;
+    obj.classes = {{1, 2}, {3, 5}, {2, 2}};
+    obj.extraStudents = 2;
+    obj.output = 0.78333;
+    return obj;
+  }
+
+  TestCase testB() {
+    TestCase obj;
+    obj.classes = {{2, 4}, {3, 9}, {4, 5}, {2, 10}};
+    obj.extraStudents = 4;
+    obj.output = 0.53485;
+    return obj;
+  }
+};
+
+class Solution {
+public:
+  double maxAverageRatio(std::vector<std::vector<int>> &classes,
+                         int extraStudents) {
+    int size = classes.size();
+    std::vector<std::pair<int, double>> diff{};
+
+    // distille class
+    std::pair<double, double> passRatio;
+    for (int i{0}; i < size; ++i) {
+      passRatio.first = classes[i][0];
+      passRatio.second = classes[i][1];
+
+      diff.push_back({i, (((passRatio.first + 1.0) / (passRatio.second + 1.0)) -
+                          (passRatio.first / passRatio.second))});
+    }
+
+    // sort from greatest to least
+    std::sort(diff.begin(), diff.end(),
+              [](std::pair<int, double> a, std::pair<int, double> b) {
+                return a.second > b.second;
+              });
+
+    // consider extraStudents to maximize
+    int pos, begin, end;
+    double pass, total;
+    for (int i{0}; i < extraStudents; ++i) {
+      // TEST:
+      for (std::pair<double, double> i : diff) {
+        std::cout << i.first << ' ' << i.second << '\n';
+      }
+      std::cout << std::endl;
+      pos = diff.front().first;
+
+      // change values
+      pass = ++classes[pos][0];
+      total = ++classes[pos][1];
+
+      // re-insert in non-increasing order
+      diff.push_back({diff.front().first, (pass / total)});
+      diff.erase(diff.begin());
+
+      std::sort(diff.begin(), diff.end(),
+                [](std::pair<int, double> a, std::pair<int, double> b) {
+                  return a.second > b.second;
+                });
+    }
+
+    // get avg pass ratio
+    double avg{
+        std::accumulate(diff.begin(), diff.end(), 0.0,
+                        [](double currentSum, std::pair<int, double> val) {
+                          return (currentSum + val.second);
+                        })};
+
+    return (avg / size);
+  }
+};
+
+int main() {
+  Test testCur{Test()};
+  Solution solCur{Solution()};
+
+  Test::TestCase unit{testCur.testB()};
+  double output{solCur.maxAverageRatio(unit.classes, unit.extraStudents)};
+
+  if (output == unit.output)
+    std::cout << "Success!\n";
+  else {
+    std::cout << "Failed!\n";
+    std::cout << "Expected: " << unit.output << '\n';
+    std::cout << "Actual: " << output << '\n';
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
