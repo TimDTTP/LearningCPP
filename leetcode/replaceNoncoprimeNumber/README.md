f482d0945693ad45cef2b761278c593b14776f2e
TimDTTP <timdttp@gmail.com>
2025-09-16 01:56:48 -0500

    finished
    Runtime:
    2150ms beats 5.09%
    Memory:
    120.97mB beats 99.07%

--- leetcode/replaceNoncoprimeNumber/README.md
+++ leetcode/replaceNoncoprimeNumber/README.md
@@ -18,7 +18,22 @@ coprime.
 - 1 <= nums\[i] <= 10^5
 
 ## Approach
--
+- Iterate through nums
+- Check if the nums\[i] and nums\[i + 1] are non-coprime
+- If yes,
+    - vector.erase\(nums.begin + \(i + 1))
+    - vector\[i] = LCM
+    - i--
+
+To find LCM, use Euclid's algorithm. From there you can find GCF with the
+formula LCM\(a,b) = \(a*b)/GCF\(a,b)
+
+Runtime:
+2150ms beats 5.09%
+Memory:
+120.97mB beats 99.07%
 
 ## Improvements
-
+Instead of using vector::erase, prefer instantiating a different storage
+instead.
+Use Euclid recursively rather than multiple calls.
--- leetcode/replaceNoncoprimeNumber/main.cpp
+++ leetcode/replaceNoncoprimeNumber/main.cpp
@@ -13,6 +13,10 @@ public:
 
   TestCase testB() { return {{2, 2, 1, 1, 3, 3, 3}, {2, 1, 1, 3}}; }
 
+  TestCase testC() {
+    return {{287, 41, 49, 287, 899, 23, 23, 20677, 5, 825}, {2009, 20677, 825}};
+  }
+
   void printVec(std::vector<int> arr) {
     std::cout << "[";
     for (int i : arr) {
@@ -24,9 +28,34 @@ public:
 
 class Solution {
 public:
+  int lcm(int a, int b) {
+    // euclid's algo
+    int greater{a > b ? a : b};
+    int lesser{a > b ? b : a};
+    int temp{};
+
+    while (greater % lesser != 0) {
+      temp = greater % lesser;
+      greater = lesser;
+      lesser = temp;
+    }
+
+    return lesser;
+  }
+
   std::vector<int> replaceNonCoprimes(std::vector<int> &nums) {
-    // useful code
-    // here
+    int multiple{};
+    for (int i{0}; i < (nums.size() - 1); i++) {
+      multiple = lcm(nums[i], nums[i + 1]);
+      if (multiple != 1) {
+        nums[i] = ((long long)nums[i] * nums[i + 1]) / multiple;
+        nums.erase(nums.begin() + (i + 1));
+        i = (i > 0) ? (i - 2) : (i - 1);
+      } else {
+      }
+    }
+
+    return nums;
   }
 };
 
@@ -34,7 +63,7 @@ int main() {
   Test testCur{Test()};
   Solution solCur{Solution()};
 
-  Test::TestCase unit{testCur.testA()};
+  Test::TestCase unit{testCur.testC()};
   std::vector<int> output{solCur.replaceNonCoprimes(unit.nums)};
 
   if (output == unit.output)
