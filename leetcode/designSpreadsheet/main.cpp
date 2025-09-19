bfb996b5392bcab2c189db68f486abbc01ff219c
TimDTTP <timdttp@gmail.com>
2025-09-19 02:31:31 -0500

    init
    #3484 Design Spreadsheet

---
+++ leetcode/designSpreadsheet/main.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+class Spreadsheet {
+public:
+  Spreadsheet(int rows) {
+    // useful
+    // code
+  }
+
+  void setCell(std::string cell, int value) {
+    // useful
+    // code
+  }
+
+  void resetCell(std::string cell) {
+    // useful
+    // code
+  }
+
+  int getValue(std::string formula) {
+    // useful
+    // code
+  }
+};
