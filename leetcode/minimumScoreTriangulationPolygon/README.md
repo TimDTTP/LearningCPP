55ff3a8b12b651d6d3b40b51e9ffb62cdf1f013c
TimDTTP <timdttp@gmail.com>
2025-09-29 18:49:14 -0500

    Failed
    TLE

--- leetcode/minimumScoreTriangulationPolygon/README.md
+++ leetcode/minimumScoreTriangulationPolygon/README.md
@@ -13,7 +13,7 @@ polygon into a set of triangles whose vertices cooicide with the vertices of
 the original polygon. This process will result in n - 2 triangles. The weight
 of the triangle will be the product of the 3 points of the triangle. Return 
 the minimum weight of the sum of triangles after performing polygon 
-triangulation.
+triangulation. Splitting the triangle cannot have overlaps.
 
 [\#1039 Minimum Score Triangulation of Polygon](https://leetcode.com/problems/minimum-score-triangulation-of-polygon/description/?envType=daily-question&envId=2025-09-29)
 
@@ -23,7 +23,13 @@ triangulation.
 - 1 <= values\[i] <= 100
 
 ## Approach
-- 
 
 ## Improvements
+Failed: TLE
+Dynamic programming problem.
+
+Runtime:
+0ms beats 100%
+Memory: 
+10.47mB beats 96.91%
 
--- leetcode/minimumScoreTriangulationPolygon/main.cpp
+++ leetcode/minimumScoreTriangulationPolygon/main.cpp
@@ -1,4 +1,6 @@
 
+#include <algorithm>
+#include <climits>
 #include <iostream>
 #include <vector>
 
@@ -14,13 +16,27 @@ public:
   TestCase testB() { return {{3, 7, 4, 5}, 144}; }
 
   TestCase testC() { return {{1, 3, 1, 4, 1, 5}, 13}; }
+
+  TestCase testD() { return {{2, 1, 4, 4}, 24}; }
 };
 
 class Solution {
 public:
-  int minScoreTriangulation(std::vector<int> values) {
-    // useful code
-    // here
+  int dp[50][50] = {};
+
+  int minScoreTriangulation(std::vector<int> &values, int i = 0, int j = 0,
+                            int res = 0) {
+    if (j == 0)
+      j = values.size() - 1;
+    if (dp[i][j] != 0)
+      return dp[i][j];
+    for (int k = i + 1; k < j; ++k) {
+      res = std::min(res == 0 ? INT_MAX : res,
+                     minScoreTriangulation(values, i, k) +
+                         values[i] * values[k] * values[j] +
+                         minScoreTriangulation(values, k, j));
+    }
+    return dp[i][j] = res;
   }
 };
 
@@ -28,7 +44,7 @@ int main() {
   Test testCur{Test()};
   Solution solCur{Solution()};
 
-  Test::TestCase unit{testCur.testA()};
+  Test::TestCase unit{testCur.testC()};
   int output{solCur.minScoreTriangulation(unit.values)};
 
   if (output == unit.output)
