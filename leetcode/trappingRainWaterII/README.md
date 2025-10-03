ee3404c288e8962578ba461ce9908fa2c75871b4
TimDTTP <timdttp@gmail.com>
2025-10-03 13:25:44 -0500

    init
    #407 Trapping Rain Water II

---
+++ leetcode/trappingRainWaterII/README.md
@@ -0,0 +1,25 @@
+
+# Problem
+Given...
+    - vector\<vector\<int>>, heightMap
+
+Return...
+    - int
+
+Where...
+- heightMap is a matrix representing the height of each unit cell on a 2D 
+elevation map. Return the volume of water it can trap after raining.
+
+[\#407 Trapping Rain Water II]()
+
+## Constraint
+- m == heightMap.length
+- n == heightMap\[i].length
+- 1 <= m, n <= 200
+- 0 <= heightMap\[i]\[j] <= 2 * 10^4
+
+## Approach
+- 
+
+## Improvements
+
---
+++ leetcode/trappingRainWaterII/main.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <vector>
+
+class Test {
+public:
+  struct TestCase {
+    std::vector<std::vector<int>> heightMap;
+    int output;
+  };
+
+  TestCase testA() {
+    return {{{1, 4, 3, 1, 3, 2}, {3, 2, 1, 3, 2, 4}, {2, 3, 3, 2, 3, 1}}, 4};
+  }
+
+  TestCase testB() {
+    return {{{3, 3, 3, 3, 3},
+             {3, 2, 2, 2, 3},
+             {3, 2, 1, 2, 3},
+             {3, 2, 2, 2, 3},
+             {3, 3, 3, 3, 3}},
+            10};
+  }
+};
+
+class Solution {
+public:
+  int trapRainWater(std::vector<std::vector<int>> &heightMap) {
+    // useful code
+    // here
+  }
+};
+
+int main() {
+  Test testCur{Test()};
+  Solution solCur{Solution()};
+
+  Test::TestCase unit{testCur.testA()};
+  int output{solCur.trapRainWater(unit.heightMap)};
+
+  if (output == unit.output)
+    std::cout << "Success!\n";
+  else {
+    std::cout << "Failed!\n";
+    std::cout << "Expected: " << unit.output << '\n';
+    std::cout << "Actual: " << output << '\n';
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
